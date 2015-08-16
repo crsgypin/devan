ffacba455f9672227d87255dbacccf2e58a08c8b
ChiuGypin <crsgypin@gmail.com>
2015-08-16 18:12:03 +0800

    modify the structure of customer page

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,20 +3,18 @@ class CustomersController < ApplicationController
 	before_action :authenticate_user!, :except=>[:index,:show]
 
 	def index
-		update_session_search
+
 		@customers = Customer.includes(:phones,:addresses=>[:city]).joins(:phones,:addresses=>[:city])
-		@customers = @customers.where(@seach_sql)
-		@customers = @customers.where("addresses.city_id = #{@customer_city}") if @customer_city >0
+		# @customers = @customers.where("addresses.city_id = #{@customer_city}") if @customer_city >0
 		@customers = @customers.where(:status=>"經營中")
 		@customers = @customers.page(params[:page]).per(30)
 
 	end
 
 	def profiles
-		update_session_search
+
 		@customers = Customer.includes(:phones,:addresses=>[:city]).joins(:phones,:addresses=>[:city])
-		@customers = @customers.where(@seach_sql)
-		@customers = @customers.where("addresses.city_id = #{@customer_city}") if @customer_city >0
+		# @customers = @customers.where("addresses.city_id = #{@customer_city}") if @customer_city >0
 		@customers = @customers.page(params[:page]).per(30)
 		# @customers = @customers.where(["customers.name like ? OR address like ? OR number LIKE ?","%#{@customer_search}%","%#{key_address}%", "%#{key_number}%"])
 	end
