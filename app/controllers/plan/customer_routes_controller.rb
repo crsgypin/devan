0755a54d14ee00f8ad35f417b0795096fe2ca72e
ChiuGypin <crsgypin@gmail.com>
2015-08-18 15:28:44 +0800

    modify sql words for index in customer route page

--- app/controllers/plan/customer_routes_controller.rb
+++ app/controllers/plan/customer_routes_controller.rb
@@ -1,17 +1,8 @@
 class Plan::CustomerRoutesController < ApplicationController
 
 	def index
-		@delivery_people = DeliveryPerson.includes(:customer_routes=>[:customer]).joins(:customer_routes=>[:customer])
-		@delivery_people.where(:status=>"在職")
-
-		if params[:wday]
-			@wday = params[:wday]
-			@delivery_people = @delivery_people.where('customer_routes.wday = ?', params[:wday].downcase)
-		else
-			@wday = "Monday"
-			@delivery_people = @delivery_people.where('customer_routes.wday = ?','monday')
-		end
-		
+		@delivery_people = DeliveryPerson.on_job.includes(:customer_routes)
+		@wday = params[:wday] ? params[:wday] : Date.today.strftime('%A')
 	end
 
 	def create
