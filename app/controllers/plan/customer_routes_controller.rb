0cdd7ca21689e3dcb950b1c5d0a7f163cebaf776
ChiuGypin <crsgypin@gmail.com>
2015-08-18 14:15:25 +0800

    complete destroy feature for customer_route

--- app/controllers/plan/customer_routes_controller.rb
+++ app/controllers/plan/customer_routes_controller.rb
@@ -20,25 +20,33 @@ class Plan::CustomerRoutesController < ApplicationController
 		@wday = params[:wday].downcase
 		@customer_route = CustomerRoute.create(:customer=>@customer, :delivery_person=>@delivery_person, :wday=>@wday)
 
-		CustomerRoute.reorder(@customer_route, 0)
+		CustomerRoute.move_order(@customer_route, 0)
 		respond_to do |format|
-			format.json {render :json=>{:template=> render_to_string(:partial=>"plan/customer_routes/route_list", :locals=>{:delivery_person=>@delivery_person})}}
+			format.json {render :json=>{:template=> render_to_string(:partial=>"plan/customer_routes/route_list", :locals=>{:delivery_person=>@delivery_person,:wday=>@wday})}}
 		end
 	end
+
+	def destroy
+		@customer_route = CustomerRoute.find(params[:id])
+		@delivery_person = @customer_route.delivery_person
+		@wday = @customer_route.wday
+		CustomerRoute.destroy_and_reorder(@customer_route)
+		respond_to do |format|
+			format.json {render :json=>{:template=> render_to_string(:partial=>"plan/customer_routes/route_list", :locals=>{:delivery_person=>@delivery_person, :wday=>@wday})}}
+		end
+	end
+
 
 	def move
 		@move_customer_route = CustomerRoute.find(params[:customer_route_id])
 		@to_index = params[:to_index].to_i
 		
-		CustomerRoute.reorder(@move_customer_route, @to_index)
+		CustomerRoute.move_order(@move_customer_route, @to_index)
 
 		respond_to do |format|
 			format.json {render :json=>{:result=>true}}
 		end
 	end
 
-	def destroy
-
-	end
 
 end
--- app/models/customer_route.rb
+++ app/models/customer_route.rb
@@ -3,7 +3,7 @@ class CustomerRoute < ActiveRecord::Base
 	belongs_to :customer
 	belongs_to :delivery_person
 
-	def self.reorder(move_customer_route, to_index)
+	def self.move_order(move_customer_route, to_index)
 		customer_routes = CustomerRoute.where(:delivery_person_id => move_customer_route.delivery_person_id, 
 																					:wday=>move_customer_route.wday).order(:row_order)
 
@@ -22,6 +22,17 @@ class CustomerRoute < ActiveRecord::Base
 
 			index +=1
 		end
+	end
 
+	def self.destroy_and_reorder(customer_route)
+		customer_routes = CustomerRoute.where(:delivery_person_id => customer_route.delivery_person_id, 
+																					:wday=>customer_route.wday).order(:row_order)
+		customer_route.destroy
+		customer_routes.each_with_index do |customer_route, index|
+			if customer_route.row_order != index
+				customer_route.row_order = index
+				customer_route.save!
+			end
+		end
 	end
 end
